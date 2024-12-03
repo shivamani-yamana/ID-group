8d6f1194d66c312d8bb02a4f17c6fbb9b08bc18a
Shivamani Yamana <shivamaniyamana2003@gmail.com>
2024-12-03 23:11:22 +0530

    Added new logo and updated navbar component

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import idstudioLogo from "../lib/images/Idstudio_logo.svg";
+  import ContactUsButton from "./ContactUsButton.svelte";
+</script>
+
+<div
+  class="flex justify-between h-24 items-center bg-white px-[80px] fixed w-full font-inter"
+>
+  <a href="/">
+    <div class="logo flex items-center justify-center" id="logo">
+      <img src={idstudioLogo} alt="Logo" class="w-40" />
+    </div>
+  </a>
+  <div class="flex gap-10 font-normal items-center justify-center">
+    <a href="#howitworks">How it works</a>
+    <a href="#services">Our Services</a>
+    <a href="#portfolio">Portfolio</a>
+    <a href="#faqs">FAQs</a>
+    <a href="#invisiblegrills">Invisible Grills</a>
+  </div>
+  <div><ContactUsButton /></div>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'inter': ['Inter Tight', 'serif'],
+        'playfairDisplay': [
+          'Playfair Display', 'serif']
+      },
+    },
   },
   plugins: [],
 }
