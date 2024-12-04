00832b933eca6f7113ea9d16f965ecb008de81cd
Shivamani Yamana <shivamaniyamana2003@gmail.com>
2024-12-05 00:54:06 +0530

    Added Our Highlights Section

---
+++ src/components/HighlightCard.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let para; // Paragraph text prop
+  export let numberText; // Number text prop
+</script>
+
+<div class="flex flex-col items-start gap-[16px] md:gap-[16px]">
+  <p
+    class="text-[#545454] font-[300] text-[12px] leading-[160%] md:text-[14px] md:leading-[160%] font-inter"
+  >
+    {para}
+  </p>
+
+  <div
+    class="text-[#292929] font-[500] text-[24px] leading-[120%] md:text-[40px] md:leading-[120%] font-inter"
+  >
+    {numberText}
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import VideoPlayer from "../components/VideoPlayer.svelte";
+  import OurHighlightsSection from "../components/OurHighlightsSection.svelte";
 </script>
 
 <svelte:head>
@@ -8,25 +9,34 @@
 </svelte:head>
 
 <section class="pt-24 w-full">
-  <div class="flex flex-col gap-10 px-4 md:px-20 w-full justify-center">
+  <div class="flex flex-col md:gap-[60px] gap-[64px]">
     <div
-      class="text-[#292929] text-center md:text-left flex md:flex-row flex-col justify-between items-center w-full md:gap-0 gap-3"
+      class="flex flex-col gap-10 px-4 pt-[7px] pb-[7px] md:pt-10 md:pb-[60px] md:px-20 w-full justify-center"
     >
-      <h1
-        class="font-playfairDisplay text-[28px] md:text-[50px] font-medium md:leading-[70px] leading-8 w-full md:w-[510px]"
+      <div
+        class="text-[#292929] text-center md:text-left flex md:flex-row flex-col justify-between items-center w-full md:gap-0 gap-3"
       >
-        Transform Your Space<br />with IDStudio
-      </h1>
-      <p
-        class="font-inter text-[14px] md:text-[20px] font-light md:leading-[32px] leading-[19.2px] w-[364px] md:w-[518px]"
+        <h1
+          class="font-playfairDisplay text-[28px] md:text-[50px] font-medium md:leading-[70px] leading-8 w-full md:w-[510px]"
+        >
+          Transform Your Space<br />with IDStudio
+        </h1>
+        <p
+          class="font-inter text-[14px] md:text-[20px] font-light md:leading-[32px] leading-[19.2px] w-[364px] md:w-[518px] text-[#707070]"
+        >
+          Elevate your home with our expert interior design solutions, from
+          invisible grills to modular kitchens and personalized interiors,
+          ensuring style and functionality in every corner.
+        </p>
+      </div>
+      <div
+        class="flex justify-center items-center self-stretch w-full md:h-[540px] overflow-clip rounded-md"
       >
-        Elevate your home with our expert interior design solutions, from
-        invisible grills to modular kitchens and personalized interiors,
-        ensuring style and functionality in every corner.
-      </p>
+        <VideoPlayer
+          videoLink="https://s3-figma-videos-production-sig.figma.com/video/1246348018179354930/TEAM/ffd0/a01b/-d551-49c2-aa7b-5f9196e602dc?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=l8hzfFyelU779viIAScNTNEsthOFbNYhqPFZlFuWNXGzWqbTl~ZIfaRTEsJOg-Qge9q5mtx5CUDoMHuYYTPe3dmOj0hbLoACKJgMwYBrcxUqgXUpFm17D1VFYLftGOiiS9UcjmpSaPShysJXQJNUevAOFvuzjo-1JmhQAtUaMZ-tSMkzbRvDXZ8xY6S6vLsXTjDMfKx2IXbXtzQcWVQDjA~TXy0sPa69O9FqPAOQkGejJDBXcy8n1BmEpDRzXZu-5y5Zo026UXzIUbGepAsDSWnlFkVJXC7RrLQZI7W4xKygbjbRUo99fVdEhiQFdS8-UAaY7EAoT9xbI8OkNDSKZw__"
+        />
+      </div>
     </div>
-    <VideoPlayer
-      videoLink="https://videos.pexels.com/video-files/4549682/4549682-hd_1920_1080_30fps.mp4"
-    />
+    <OurHighlightsSection />
   </div>
 </section>
