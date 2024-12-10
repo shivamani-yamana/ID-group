c9dac826ca0bda284d6863683ab06022a6583b4e
Aditya01229 <adityajasoriya2003@gmail.com>
2024-12-10 18:55:43 +0530

    Image path error solved

--- src/components/HowItWorks.svelte
+++ src/components/HowItWorks.svelte
@@ -23,13 +23,17 @@
   },
 ];
 
+import Img1 from "/src/lib/images/hiw.svg";
+import Img2 from "/src/lib/images/hiw_img2.svg";
+import Img3 from "/src/lib/images/hiw_img3.svg";
+
 </script>
 <div class="flex flex-col w-full gap-[24px] items-center justify-center p-[24px_0px] md:h-auto md:flex-row md:p-[60px_80px_60px_0px] md:self-stretch md:justify-start md:items-start md:gap-[100px] bg-[#DFE0AF]">
     <div class="flex flex-col gap-[12px] md:gap-[20px] md:justify-between md:item" style="max-width: fit-content;">
-        <img class="w-full h-auto" src="/src/lib/images/hiw.svg" alt="Main">
+        <img class="w-full h-auto" src="{Img1}" alt="Main">
         <div class="flex gap-3 md:gap-5">
-            <img class="w-[calc(50%-6px)] md:w-[calc(50%-10px)] h-auto" src="/src/lib/images/hiw_img2.svg" alt="Secondary 1">
-            <img class="w-[calc(50%-6px)] md:w-[calc(50%-10px)] h-auto" src="/src/lib/images/hiw_img3.svg" alt="Secondary 2">
+            <img class="w-[calc(50%-6px)] md:w-[calc(50%-10px)] h-auto" src="{Img2}" alt="Secondary 1">
+            <img class="w-[calc(50%-6px)] md:w-[calc(50%-10px)] h-auto" src="{Img3}" alt="Secondary 2">
         </div>
     </div>
     <div class="w-full p-[0px_10px] max-w-[396px] flex flex-col items-start gap-[24px] md:gap-[40px] md:max-w-[570px]">
