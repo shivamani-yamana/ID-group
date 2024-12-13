a4d887dcc4ef719e54c7352b49c2b0d7439431c4
Shivamani Yamana <shivamaniyamana2003@gmail.com>
2024-12-13 15:19:35 +0530

    Added Initial Popup Section for Consulation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,13 @@
   import "../app.css";
   import Footer from "$components/Footer.svelte";
   import Navbar from "$components/Navbar.svelte";
+  import ConsultaionPopup from "$components/ConsultaionPopup.svelte";
   let { children } = $props();
 </script>
 
 <div class="app">
+  <ConsultaionPopup />
+
   <Navbar />
   <main>
     {@render children()}
